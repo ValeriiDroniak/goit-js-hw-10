a20445940e09e46c1ae0ea07d7c136b3f5c47559
ValeriiDroniak <105050454+ValeriiDroniak@users.noreply.github.com>
2022-10-13 11:30:41 +0000

    Deploying to gh-pages from @ ValeriiDroniak/goit-js-hw-10@ddf48130acc44d55673cd0b942af0eaeec668cfc 🚀

---
+++ v-async.6931d21b.js
@@ -0,0 +1,2 @@
+!function({homeLink:e,navLinks:n}){/.html$/.test(location.href)?n.forEach((e=>{location.href===e.href&&e.classList.add("current")})):e.classList.add("current")}({homeLink:document.querySelector('nav a[href = "./"]'),navLinks:document.querySelectorAll("nav a")});
+//# sourceMappingURL=v-async.6931d21b.js.map
---
+++ v-async.d69c5aba.js
@@ -0,0 +1,2 @@
+!function(){"use strict";!function({homeLink:e,navLinks:n}){/.html$/.test(location.href)?n.forEach((e=>{location.href===e.href&&e.classList.add("current")})):e.classList.add("current")}({homeLink:document.querySelector('nav a[href = "./"]'),navLinks:document.querySelectorAll("nav a")})}();
+//# sourceMappingURL=v-async.d69c5aba.js.map
